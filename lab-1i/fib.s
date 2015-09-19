fd8d6a211345071ad5f64ad5b119214ba8000582
JIElite <ita3051@gmail.com>
2015-09-20 00:33:15 +0800

    remove r6, reducing the number of used register

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,48 +11,47 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, r6, lr}
+	push {r3, r4, r5, lr}
 		
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	subs r4, r0, #0
-	ble .L3
+	@ because r0 is as same as input, we use it as counter
+	cmp r0, #0
+	beq .L3
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4, #1
-	beq .L4	
+	cmp r0, #1
+	beq .L4
 
 	@ If input number is bigger than 1
 	@ R4 is used to be counter 
 
 	mov r3, #0
-	mov r5, #1
+	mov r4, #1
 
 .L5:
-	mov r6, r5
-	add r5, r3, r5 
-	mov r3, r6
+	@ use r5 as tmp variable
+	mov r5, r4
+	add r4, r4, r3
+	mov r3, r5
 	
-	sub r4, r4, #1
-	cmp r4, #1
+	@ treat r0 as counter
+	sub r0, r0, #1
+	cmp r0, #1
 	bne .L5
 
-	mov r0, r5
+	mov r0, r4
 		
-	pop {r3, r4, r5, r6, pc}		@EPILOG
+	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 
 .L3:
 	@ return value is r0
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, r6, pc}		@ EPILOG
+	pop {r3, r4, r5, pc}		@ EPILOG
 
 .L4:
 	@ return value is r0
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, r6, pc}		@ EPILOG
+	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
