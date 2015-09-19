1ac279e8c3d12543e6a90775b945e2fdd40d0c1f
JIElite <ita3051@gmail.com>
2015-09-19 22:39:50 +0800

    finish iterative fibonacci with addtional register r6

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,48 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
-
+	push {r3, r4, r5, r6, lr}
+		
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs r4, r0, #0
+	ble .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+	cmp r4, #1
+	beq .L4	
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	@ If input number is bigger than 1
+	@ R4 is used to be counter 
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+	mov r3, #0
+	mov r5, #1
 
-	@ R0 = R5 + R0 (update flags)
+.L5:
+	mov r6, r5
+	add r5, r3, r5 
+	mov r3, r6
+	
+	sub r4, r4, #1
+	cmp r4, #1
+	bne .L5
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	mov r0, r5
+		
+	pop {r3, r4, r5, r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
+
 .L3:
+	@ return value is r0
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r3, r4, r5, r6, pc}		@ EPILOG
 
 .L4:
+	@ return value is r0
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r3, r4, r5, r6, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
