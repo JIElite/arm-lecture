306d42c28491fc4289cdc86a864a358dd12bea2d
JIElite <ita3051@gmail.com>
2015-09-22 13:43:14 +0800

    update performance graph

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
-
+#include <time.h>
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
+  	int number=0;
+  	int result=0;
+	
+	FILE* fptr = fopen("fib_i.txt", "a");
+  	scanf("%d",&number);
+  	
+	clock_t start = clock();
+	result = fibonacci(number);
+	clock_t end = clock();
 
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+	// calculate elapsed time
+	double elapsed_time = (double)(end - start);
+	fprintf(fptr, "%d\t%f\n", number, elapsed_time);
+	fclose(fptr);
+
+
+  	return 0;
 }
 
