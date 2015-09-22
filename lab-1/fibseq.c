1d4a59fe8c20081434d743a8f23d1cfa550f18f5
JIElite <ita3051@gmail.com>
2015-09-22 16:54:53 +0800

    create performance graph for recursive version

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
+  int number = 0;
+  int result = 0;
 
   scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+  
+  	clock_t start = clock();
+  	result = fibonacci(number);
+	clock_t end = clock();
+	
+	FILE *fptr = fopen("fib.txt", "a");
+	if (fptr != NULL){
+		double elapsed_time = (double)(end - start) / CLOCKS_PER_SEC;
+		fprintf(fptr, "%d\t%f\n", number, elapsed_time);
+		fclose(fptr);
+	}
+
+  	printf("The fibonacci sequence at %d is: %d\n", number, result);
+	
+	return 0;
 }
 
