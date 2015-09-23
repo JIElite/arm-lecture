41006ce4e4036d3248e2a8d3e53528b3c3c81c7c
JIElite <ita3051@gmail.com>
2015-09-24 00:05:33 +0800

    update tail recursion version

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,11 +11,12 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3, lr}
+
 
 	cmp r0, #0
 	beq .L3
-	
+
 	mov r3, r2
 	add r2, r1, r2
 	mov r1, r3
@@ -24,16 +25,12 @@ fibonacci:
 	mov r0, r1
 
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r3, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
-	mov r0, r1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	mov r0, #0
+	pop {r3, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
